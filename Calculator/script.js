d77bc3d2a0c684b1e35f535d9ed7ebc68caf04d9
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2022-06-27 18:35:34 -0400

    some updates

--- Calculator/script.js
+++ Calculator/script.js
@@ -40,26 +40,39 @@ let previous = 0;
 let current = 0;
 let operand = undefined;
 //adding listeners to calculator buttons to show on display, 
+//displaying numbers, changing scale on hover but it doesnt work completely yet
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
     number.addEventListener('click',displayNumber);
     number.addEventListener('mouseover',scale); //need to fix
 })
+//when an operator is pressed
 const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
     operator.addEventListener('click',moveCurrent);
 })
+//clear button listener
+const clears = document.querySelectorAll('.clear');
+clears.forEach((clear) => {
+    clear.addEventListener('click',clearNum);
+})
+
+
+
+//displaying a number when the listener is activated
 function displayNumber() {
     if (current === 0) {
         current = this.id;
-    }
+    } 
     else {
         current = "" + current + this.id;
     }
     const currentDisplay = document.querySelector('.current');
     currentDisplay.innerHTML = current;
-    return current;
+    
 }
+//moving the current number to the previous when operation is pressed
+//NEED TO STORE THE OPERATOR SOMEHOW TO DO THE MATH, maybe add an id and if statements
 function moveCurrent() {
     previous = current;
     current = 0;
@@ -72,4 +85,16 @@ function moveCurrent() {
 //need to fix
 function scale() {
     this.style.scale = '1.2';
+}
+
+//clear function
+function clearNum() {
+    current = 0;
+    previous = 0;
+    operand = undefined;
+    const currentDisplay = document.querySelector('.current');
+    currentDisplay.innerHTML = current;
+    const previousDisplay = document.querySelector('.previous');
+    previousDisplay.innerHTML = previous + this.id;
+    //moveCurrent();
 }
